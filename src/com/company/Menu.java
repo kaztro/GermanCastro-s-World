de9ddee8cf3181dbcb21a5dcd2c8d6c6a22b0d29
Kaztro <00229017@uca.edu.sv>
2018-06-07 15:19:17 -0600

    Working on the menu

---
+++ src/com/company/Menu.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Menu {
+    public void options() {
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("|                   WELCOME TO GERMAN'S WOLRD!                       |");
+        System.out.println("|Select one of the races and write your nickname:                    |");
+        System.out.println("|1. Aliens                                                           |");
+    }
+}
