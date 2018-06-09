c3960c295507236c62bcddadc319bb79c4a9d120
Kaztro <00229017@uca.edu.sv>
2018-06-09 01:31:21 -0600

    Lista are not finished, still workin on AliensArmyList...

--- src/List/AliensArmyList.java
+++ src/List/AliensArmyList.java
@@ -1,36 +1,39 @@
 package List;
 
 import Estructures.Army.AliensArmy;
+import Estructures.Army.Army;
+import Factory.AbstractFactory;
+import Factory.FactoryProducer;
 
 import java.util.ArrayList;
 
-public class AliensArmyList {
-    /*private ArrayList<AliensArmy> army;
+public class AliensArmyList implements List {
+    private ArrayList<AliensArmy> army;
 
     public AliensArmyList() {
         army = new ArrayList<>();
     }
 
-    public void add() {
-        AliensArmy aArmy = new AliensArmy();
-
-        army.add(aArmy);
-
-        //Scanner read = new Scanner(System.in);
-
-        System.out.println("Tu soldado ha sido agregado!");
-        aArmy.build();
+    @Override
+    public void addArmy() {
+        AbstractFactory factory;
+        factory = FactoryProducer.getFactory("army");
+        Army armyestruct = factory.getArmy("aliensarmy");
     }
 
-    public void add(AliensArmy aArmy) throws Exception {
+    public void addArmy(AliensArmy aArmy) throws Exception {
         if(aArmy != null) {
-            if (!army.contains(aArmy)){ army.add(aArmy);}
-            Exception e = new Exception("You are sending clones!");
-            throw e;
-        } else{ throw new Exception("You're sending me phantom soldiers, we can't fight with that!!");}
+            if (!army.contains(aArmy)){ army.add(aArmy);} else {
+                Exception e = new Exception("You are sending clones!");
+                throw e;
+            }
+        } else {
+            throw new Exception("You're sending me phantom soldiers, we can't fight with that!!");
+        }
     }
 
+    @Override
     public void show() {
-
-    }*/
+        System.out.println("Hasta aqui te quedaste we :'v ve a dormir...");
+    }
 }
---
+++ src/List/List.java
@@ -0,0 +1,8 @@
+package List;
+
+public interface List {
+    public void addArmy();
+
+    public void show();
+    //public void modify();
+}
