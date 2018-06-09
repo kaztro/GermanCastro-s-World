922b60f9bb096d5851f68c07a3210e4f1857317b
Kaztro <00229017@uca.edu.sv>
2018-06-09 01:03:21 -0600

    Still working in the class menu and some new "lists"...

--- src/List/AliensArmyList.java
+++ src/List/AliensArmyList.java
@@ -5,7 +5,7 @@ import Estructures.Army.AliensArmy;
 import java.util.ArrayList;
 
 public class AliensArmyList {
-    private ArrayList<AliensArmy> army;
+    /*private ArrayList<AliensArmy> army;
 
     public AliensArmyList() {
         army = new ArrayList<>();
@@ -27,10 +27,10 @@ public class AliensArmyList {
             if (!army.contains(aArmy)){ army.add(aArmy);}
             Exception e = new Exception("You are sending clones!");
             throw e;
-        } else{ throw new Exception("You'r sending me phantom soldiers, we can't fight with that!!");}
+        } else{ throw new Exception("You're sending me phantom soldiers, we can't fight with that!!");}
     }
 
     public void show() {
 
-    }
+    }*/
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -7,17 +7,37 @@ import Player.Player;
 import Player.PlayerBuilder;
 import Player.ConcretePlayer;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
+    private static Menu menu;
+
+    private Menu() {
+    }
+
+    public static Menu getInstance() {
+        if (menu == null) {
+            menu = new Menu();
+        }
+        return menu;
+    }
+
     public void options() {
-        System.out.println("---------------------------------------------------------------------");
-        System.out.println("|                   WELCOME TO GERMAN'S WOLRD!                       |");
-        System.out.println("|Select one of the races and write your nickname:                    |");
-        System.out.println("|                                                           |");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("¦                   WELCOME TO GERMAN'S WOLRD!                         ¦");
+        System.out.println("¦1.Select one of the races and write your nickname for player 1 and 2: ¦");
+        System.out.println("¦2.Build Energy Estructure                                             ¦");
+        System.out.println("¦3.Build Money Estructure                                              ¦");
+        System.out.println("¦4.Build Material Estructure                                           ¦");
+        System.out.println("¦5.Build Army Estructure                                               ¦");
+        System.out.println("¦6.Build Vehicles                                                      ¦");
+        System.out.println("¦7.Exit                                                                ¦");
+        System.out.println("-----------------------------------------------------------------------");
     }
 
     public void game() {
+
         Player avatar = new Player();
         PlayerBuilder p1 = new ConcretePlayer();
 
@@ -33,17 +53,29 @@ public class Menu {
 
         Scanner scanner = new Scanner(System.in);
 
-        int op = scanner.nextInt();
+        int op = 0;
 
-        switch (op) {
-            case 1:
-                p1.buildName();
-                p1.buildEarth();
+        while (op != 7) {
 
-                p2.buildName();
-                p2.buildEarth();
+            options();
+            try {
+                op = scanner.nextInt();
 
+                switch (op) {
+                    case 1:
+                        p1.buildName();
+                        p1.buildEarth();
 
+                        p2.buildName();
+                        p2.buildEarth();
+
+                    default:
+                        System.out.println("The number you type It's not part of the options or maybe It's not a number.\nPlease read the menu");
+                }
+            } catch (InputMismatchException E) {
+                System.err.println("The number you've type It's not an Int");
+            }
+            scanner.nextLine();
         }
     }
 }
